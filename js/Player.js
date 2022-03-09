b889db4053184a9de761635dad82bd838379d861
Davco-30 <94723862+Davco-30@users.noreply.github.com>
2022-03-08 18:01:11 -0600

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,86 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+    this.rank = 0;
+    this.fuel = 185;
+    this.life = 185;
+    this.score = 0;
+  }
+
+  addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score
+    });
+  }
+
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
+
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).update({
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score
+    });
+  }
+
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+    });
+  }
+
+  //Lee el valor de CarsAtEnd dentro de la base de datos de Firebase
+  getCarsAtEnd() {
+    //Hacemos referencia con nuetsra base de datos.
+    database.ref("carsAtEnd").on("value", data => {
+      //Esto también evalúa y actualiza nuestra base de datos
+      this.rank = data.val();
+    });
+  }
+
+  //Esta función e estática.
+  //Lee y actualiza el valor de CarsAtEnd
+  static updateCarsAtEnd(rank) {
+    database.ref("/").update({
+      carsAtEnd: rank
+    });
+  }
+}
